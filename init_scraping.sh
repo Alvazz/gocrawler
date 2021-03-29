e9a30b1ce5f3b2954472f8b910709708401d89bf
Leonardo Arellano Martinez <leo.aremtz98@gmail.com>
2021-03-29 10:56:45 -0600

    Se crea el archivo de bash para poder rotar la IP e iniciar el web scraping

---
+++ init_scraping.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+VPN_CONFIGFILES=("sweden" "uk_manchester" "uk_london" "spain" "uk_southampton" "us_california" "mexico" "us_silicon_valley" "us_east" "us_west")
+
+test_connection() {
+	local IP_INFO=$(curl -s -m 5 ipinfo.io/json)
+	while [ $? -ne 0 ]; do
+		echo "Offline"
+		IP_INFO=$(curl -s -m 5 ipinfo.io/json)
+	done
+	echo "Online"
+	echo $IP_INFO
+	local IP=$(echo "$IP_INFO" | jq --raw-output '.ip')
+	eval "$1=$IP"
+}
+
+connect_to_vpn() {
+	local len=${#VPN_CONFIGFILES[@]}
+	a=$( shuf -i 0-$(expr $len - 1))
+	for i in ${a[@]}; do
+		local CURRENT_CONFIG=${VPN_CONFIGFILES[$i]}
+		echo "$i $CURRENT_CONFIG"
+		systemctl start pia@$CURRENT_CONFIG
+		local IP_VPN=""
+		test_connection IP_VPN
+		echo "Nueva IP: $IP_VPN | IP Anterior: $DEFAULT_IP "
+		while [ "$DEFAULT_IP" = "$IP_VPN" ]; do
+			echo "Probando nuevamente..."
+			sleep 2
+			test_connection IP_VPN
+			echo $IP_VPN
+		done
+        echo "Conectado a $CURRENT_CONFIG"
+        sleep 3
+		clear
+		crawler
+
+		systemctl stop pia@$CURRENT_CONFIG
+		test_connection IP_VPN
+		echo $IP_VPN
+		while [ "$DEFAULT_IP" != "$IP_VPN" ]; do
+			echo "Desconectando nuevamente..."
+			sleep 2
+			test_connection IP_VPN
+			echo $IP_VPN
+		done
+        echo "Desconectado de $CURRENT_CONFIG"
+        sleep 3
+		clear
+	done
+}
+
+DEFAULT_IP=""
+test_connection DEFAULT_IP
+echo "IP DEFAULT: $DEFAULT_IP"
+connect_to_vpn
+echo "IP_VPN: $IP_VPN IP: $IP"
+
